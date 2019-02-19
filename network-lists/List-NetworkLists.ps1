7aeb785f11888be2391556e51f9234e847f2bcd7
Stuart Macleod <smacleod@akamai.com>
2019-02-19 10:25:55 +0000

    Adding network lists section

---
+++ network-lists/List-NetworkLists.ps1
@@ -0,0 +1,27 @@
+function List-NetworkLists
+{
+    Param(
+        [Parameter(Mandatory=$false)] [string] $Section = 'default',
+        [Parameter(Mandatory=$false)] [string] $AccountSwitchKey,
+        [Parameter(Mandatory=$false)] [switch] $Extended,
+        [Parameter(Mandatory=$false)] [switch] $IncludeElements,
+        [Parameter(Mandatory=$false)] [string] $ListType = "IP",
+        [Parameter(Mandatory=$false)] [string] $Search
+    )
+
+    # Check creds
+    $Credentials = Get-AKCredentialsFromRC -Section $Section
+    if(!$Credentials){ return $null }
+
+    $ReqURL = "https://" + $Credentials.host + "/network-list/v2/network-lists?extended=$Extended&includeElements=$IncludeElements&listType=$ListType"
+    if($Search) { $ReqURL += "&search=$Search"}
+    if($AccountSwitchKey)
+    {
+        $ReqURL += "&accountSwitchKey=$AccountSwitchKey"
+    }
+
+    $Result = Invoke-AkamaiOPEN -Method GET -ClientToken $Credentials.client_token -ClientAccessToken $Credentials.access_token -ClientSecret $Credentials.client_secret -ReqURL $ReqURL
+
+    return $Result.networkLists       
+}
+
