7acbd736ef319eac295b46f93d552519a694d083
Stuart Macleod <smacleod@akamai.com>
2019-04-12 11:36:35 +0100

    Some new functions and README tweaks

---
+++ identity-management/Get-PropertyResources.ps1
@@ -0,0 +1,24 @@
+function Get-PropertyResources
+{
+    Param(
+        [Parameter(Mandatory=$true)] [string] $PropertyID,
+        [Parameter(Mandatory=$true)] [string] $GroupID,
+        [Parameter(Mandatory=$false)] [string] $Section = 'default',
+        [Parameter(Mandatory=$false)] [string] $AccountSwitchKey
+    )
+
+    # Check creds
+    $Credentials = Get-AKCredentialsFromRC -Section $Section
+    if(!$Credentials){ return $null }
+
+    $ReqURL = "https://" + $Credentials.host + "/identity-management/v2/user-admin/properties/$PropertyID/resources?groupId=$GroupID&accountSwitchKey=$AccountSwitchKey"
+    
+    try {
+        $Result = Invoke-AkamaiOPEN -Method GET -ClientToken $Credentials.client_token -ClientAccessToken $Credentials.access_token -ClientSecret $Credentials.client_secret -ReqURL $ReqURL
+        return $Result
+    }
+    catch {
+        throw $_.Exception 
+    }
+}
+
---
+++ identity-management/List-IDMProperties.ps1
@@ -0,0 +1,23 @@
+function List-IDMProperties
+{
+    Param(
+        [Parameter(Mandatory=$false)] [string] $GroupID,
+        [Parameter(Mandatory=$false)] [string] $Section = 'default',
+        [Parameter(Mandatory=$false)] [string] $AccountSwitchKey
+    )
+
+    # Check creds
+    $Credentials = Get-AKCredentialsFromRC -Section $Section
+    if(!$Credentials){ return $null }
+
+    $ReqURL = "https://" + $Credentials.host + "/identity-management/v2/user-admin/properties?groupId=$GroupID&accountSwitchKey=$AccountSwitchKey"
+    
+    try {
+        $Result = Invoke-AkamaiOPEN -Method GET -ClientToken $Credentials.client_token -ClientAccessToken $Credentials.access_token -ClientSecret $Credentials.client_secret -ReqURL $ReqURL
+        return $Result
+    }
+    catch {
+        throw $_.Exception 
+    }
+}
+
