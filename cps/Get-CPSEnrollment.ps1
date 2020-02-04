8862ef7519bfbc1eec8d1bb15e10c5b6aeec23b6
Stuart Macleod <heystuartmacleod@gmail.com>
2020-02-04 17:51:47 +0000

    Removing unused options and adding better ones

--- cps/Get-CPSEnrollment.ps1
+++ cps/Get-CPSEnrollment.ps1
@@ -9,7 +9,7 @@ function Get-CPSEnrollment
 
     $Path = "/cps/v2/enrollments/$EnrollmentID`?accountSwitchKey=$AccountSwitchKey"
     $AdditionalHeaders = @{
-        'accept' = 'application/vnd.akamai.cps.enrollments.v7+json'
+        'accept' = 'application/vnd.akamai.cps.enrollment.v7+json'
     }
 
     try {
--- property/Set-PropertyHostnames.ps1
+++ property/Set-PropertyHostnames.ps1
@@ -1,11 +1,10 @@
 function Set-PropertyHostnames
 {
     Param(
-        [Parameter(Mandatory=$true)]  [string] $PropertyId,
+        [Parameter(ParameterSetName="name", Mandatory=$true)]  [string] $PropertyName,
+        [Parameter(ParameterSetName="id", Mandatory=$true)]  [string] $PropertyId,
         [Parameter(Mandatory=$true)]  [string] $PropertyVersion,
-        [Parameter(ParameterSetName='attributes', Mandatory=$false)]  [string[]] $Hostnames,
-        [Parameter(ParameterSetName='attributes', Mandatory=$false)]  [string[]] $EdgeHostnames,
-        [Parameter(ParameterSetName='postbody', Mandatory=$false)]  [string] $Body,
+        [Parameter(Mandatory=$false)] [string] $Body,
         [Parameter(Mandatory=$false)] [string] $GroupID,
         [Parameter(Mandatory=$false)] [string] $ContractId,
         [Parameter(Mandatory=$false)] [switch] $ValidateHostnames,
@@ -14,45 +13,28 @@ function Set-PropertyHostnames
         [Parameter(Mandatory=$false)] [string] $AccountSwitchKey
     )
 
+    # Find property if user has specified PropertyName
+    if($PropertyName){
+        try{
+            $Property = Find-Property -PropertyName $PropertyName -latest -EdgeRCFile $EdgeRCFile -Section $Section -AccountSwitchKey $AccountSwitchKey
+            $PropertyID = $Property.propertyId
+            if($PropertyID -eq ''){
+                throw "Property '$PropertyName' not found"
+            }
+        }
+        catch{
+            throw $_.Exception
+        }
+    }
+
+    if($PropertyVersion -eq "latest")
+    {
+        $PropertyVersion = (Get-Property -PropertyId $PropertyId -EdgeRCFile $EdgeRCFile -Section $Section -AccountSwitchKey $AccountSwitchKey).latestVersion
+    }
+
     # Nullify false switches
     $ValidateHostnamesString = $ValidateHostnames.IsPresent.ToString().ToLower()
     if(!$ValidateHostnames){ $ValidateHostnamesString = '' }
-
-    if($PSCmdlet.ParameterSetName -eq 'attributes')
-    {
-        $BodyObj = New-Object System.Collections.ArrayList
-
-        # If number of hostnames does not match number of edges, but number of edges is 1 share edge between all
-        if($Hostnames.Count -ne 1 -and $EdgeHostnames.Count -eq 1){
-            $Hostnames | foreach {
-                $BodyObj.Add(
-                    @{
-                        cnameType = "EDGE_HOSTNAME"
-                        cnameFrom = $_
-                        cnameTo = $EdgeHostnames[0]
-                    }
-                ) | Out-Null
-            }
-        }
-        # if numbers do not match and number of edges is not 1, throw an error
-        elseif($Hostnames.Count -ne $EdgeHostnames.Count){
-            throw "Number of EdgeHostnames should equal number of Hostnames, or be 1 only which is shared"
-        }
-        # otherwise match hostnames with edges of the same index
-        else{
-            for($i = 0; $i -lt $Hostnames.Count; $i++ ) {
-                $BodyObj.Add(
-                    @{
-                        cnameType = "EDGE_HOSTNAME"
-                        cnameFrom = $Hostnames[$i]
-                        cnameTo = $EdgeHostnames[$i]
-                    }
-                ) | Out-Null
-            }
-        }
-
-        $Body = $BodyObj | ConvertTo-Json -Depth 100
-    }
 
     $Path = "/papi/v1/properties/$PropertyId/versions/$PropertyVersion/hostnames?contractId=$ContractId&groupId=$GroupID&validateHostnames=$ValidateHostnamesString&accountSwitchKey=$AccountSwitchKey"
 
