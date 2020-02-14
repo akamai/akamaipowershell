846f76e7d776edcd1ab38dc634f9d8f93516e48c
Stuart Macleod <heystuartmacleod@gmail.com>
2020-02-14 17:17:16 +0000

    Ease of new version

--- property/New-PropertyVersion.ps1
+++ property/New-PropertyVersion.ps1
@@ -1,8 +1,9 @@
 function New-PropertyVersion
 {
     Param(
-        [Parameter(Mandatory=$true)] [string] $PropertyId,
-        [Parameter(ParameterSetName='attributes', Mandatory=$false)]  [int] $CreateFromVersion,
+        [Parameter(Mandatory=$false)] [string] $PropertyName,    
+        [Parameter(Mandatory=$false)] [string] $PropertyId,
+        [Parameter(ParameterSetName='attributes', Mandatory=$false)]  [string] $CreateFromVersion,
         [Parameter(ParameterSetName='attributes', Mandatory=$false)]  [string] $CreateFromEtag,
         [Parameter(ParameterSetName='postbody', Mandatory=$false)]  [string] $Body,
         [Parameter(Mandatory=$false)] [string] $GroupID,
@@ -11,19 +12,58 @@ function New-PropertyVersion
         [Parameter(Mandatory=$false)] [string] $Section = 'papi',
         [Parameter(Mandatory=$false)] [string] $AccountSwitchKey
     )
-
-    $Path = "/papi/v1/properties/$PropertyId/versions?contractId=$ContractId&groupId=$GroupID&accountSwitchKey=$AccountSwitchKey"
     
+    if($PropertyName -eq '' -and $PropertyId -eq ''){
+        throw "You must specify either a PropertyName or a PropertyID"
+    }
+
+    if($PropertyName){
+        try{
+            $Property = Find-Property -PropertyName $PropertyName -latest -EdgeRCFile $EdgeRCFile -Section $Section -AccountSwitchKey $AccountSwitchKey
+            $PropertyID = $Property.propertyId
+            if($PropertyID -eq ''){
+                throw "Property '$PropertyName' not found"
+            }
+        }
+        catch{
+            throw $_.Exception
+        }
+    }
+
     if($PSCmdlet.ParameterSetName -eq 'attributes')
     {
         if($CreateFromVersion -eq '' -and $CreateFromEtag -eq '')
         {
             return "If specifying attributes you must provide at least one of: CreateFromVersion, CreateFromEtag"
         }
-        $PostObject = @{"createFromVersion"=$CreateFromVersion}
+
+        if($CreateFromVersion){
+            if($CreateFromVersion.ToLower() -eq "latest"){
+                try{
+                    if($PropertyName){
+                        $CreateFromVersion = $Property.propertyVersion
+                    }
+                    else{
+                        $Property = Get-Property -PropertyId $PropertyID -GroupID $GroupID -ContractId $ContractId -EdgeRCFile $EdgeRCFile -Section $Section -AccountSwitchKey $AccountSwitchKey
+                        $CreateFromVersion = $Property.latestVersion
+                    }
+                }
+                catch{
+                    throw $_.Exception
+                }
+            }
+    
+            $PostObject = @{"createFromVersion"=$CreateFromVersion}
+        }
+        
+        if($CreateFromEtag){
+            $PostObject = @{"createFromEtag"=$CreateFromEtag}
+        }
+
         $Body = $PostObject | Convertto-json
     }
     
+    $Path = "/papi/v1/properties/$PropertyId/versions?contractId=$ContractId&groupId=$GroupID&accountSwitchKey=$AccountSwitchKey"
     
     try {
         $Result = Invoke-AkamaiRestMethod -Method POST -Path $Path -EdgeRCFile $EdgeRCFile -Section $Section -Body $Body
