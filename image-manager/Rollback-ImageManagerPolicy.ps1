8e41b47b398be8ac17478c0d1e811f4fd96494d0
Stuart Macleod <heystuartmacleod@gmail.com>
2021-08-20 12:22:24 +0100

    Added rollback endpoint

---
+++ image-manager/Rollback-ImageManagerPolicy.ps1
@@ -0,0 +1,29 @@
+function Rollback-ImageManagerPolicy
+{
+    Param(
+        [Parameter(Mandatory=$true)]  [string] $PolicySetAPIKey,
+        [Parameter(Mandatory=$true)]  [string] $PolicyID,
+        [Parameter(Mandatory=$true)]  [string] [ValidateSet('Staging', 'Production')] $Network,
+        [Parameter(Mandatory=$false)] [string] $ContractID,
+        [Parameter(Mandatory=$false)] [string] $EdgeRCFile = '~\.edgerc',
+        [Parameter(Mandatory=$false)] [string] $Section = 'default',
+        [Parameter(Mandatory=$false)] [string] $AccountSwitchKey
+    )
+
+    $Network = $Network.ToLower()
+    $Path = "/imaging/v2/network/$Network/policies/rollback/$PolicyID`?accountSwitchKey=$AccountSwitchKey"
+    $AdditionalHeaders = @{ 'Luna-Token' = $PolicySetAPIKey }
+
+    if($ContractID -ne ''){
+        $AdditionalHeaders['Contract'] = $ContractID
+    }
+
+    try {
+        $Result = Invoke-AkamaiRestMethod -Method PUT -Path $Path -AdditionalHeaders $AdditionalHeaders -EdgeRCFile $EdgeRCFile -Section $Section
+        return $Result
+    }
+    catch {
+        throw $_.Exception
+    }
+}
+
