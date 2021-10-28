01d50ada4f851d0285c3927901e6cbc880a24841
Stuart Macleod <heystuartmacleod@gmail.com>
2021-10-28 10:23:56 +0100

    Added load balancer options

--- cloudlets/Set-CloudletLoadBalancingVersion.ps1
+++ cloudlets/Set-CloudletLoadBalancingVersion.ps1
@@ -3,28 +3,41 @@ function Set-CloudletLoadBalancingVersion
     Param(
         [Parameter(Mandatory=$true)]  [string] $OriginID,
         [Parameter(Mandatory=$true)]  [string] $Version,
-        [Parameter(Mandatory=$true)]  [string] $Body,
+        [Parameter(Mandatory=$true,ParameterSetName="pipeline",ValueFromPipeline=$true)]  [object] $LoadBalancer,
+        [Parameter(Mandatory=$true,ParameterSetName="postbody")]  [string] $Body,
         [Parameter(Mandatory=$false)] [switch] $Validate,
         [Parameter(Mandatory=$false)] [string] $EdgeRCFile = '~\.edgerc',
         [Parameter(Mandatory=$false)] [string] $Section = 'default',
         [Parameter(Mandatory=$false)] [string] $AccountSwitchKey
     )
 
-    # nullify false switches
-    $ValidateString = $Validate.IsPresent.ToString().ToLower()
-    if(!$Validate){ $ValidateString = '' }
+    begin {}
+
+    process{
+        # nullify false switches
+        $ValidateString = $Validate.IsPresent.ToString().ToLower()
+        if(!$Validate){ $ValidateString = '' }
+        
+        $Path = "/cloudlets/api/v2/origins/$OriginID/versions/$Version`?validate=$ValidateString&accountSwitchKey=$AccountSwitchKey"
+
+        $AdditionalHeaders = @{
+            'Content-Type' = 'application/json'
+        }
+
+        if($LoadBalancer){
+            $Body = $LoadBalancer | ConvertTo-Json -Depth 100
+        }
+
+        try {
+            $Result = Invoke-AkamaiRestMethod -Method PUT -Path $Path -Body $Body -AdditionalHeaders $AdditionalHeaders -EdgeRCFile $EdgeRCFile -Section $Section
+            return $Result
+        }
+        catch {
+            throw $_.Exception
+        }
+    }
+
+    end{}
+
     
-    $Path = "/cloudlets/api/v2/origins/$OriginID/versions/$Version`?validate=$ValidateString&accountSwitchKey=$AccountSwitchKey"
-
-    $AdditionalHeaders = @{
-        'Content-Type' = 'application/json'
-    }
-
-    try {
-        $Result = Invoke-AkamaiRestMethod -Method PUT -Path $Path -Body $Body -AdditionalHeaders $AdditionalHeaders -EdgeRCFile $EdgeRCFile -Section $Section
-        return $Result
-    }
-    catch {
-        throw $_.Exception
-    }
 }
