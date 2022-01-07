fc073fd9f2853806424bffedd4cbbcb55a52a503
Stuart Macleod <heystuartmacleod@gmail.com>
2022-01-07 15:21:03 +0000

    Network list update improvements

---
+++ network-lists/RemoveFrom-NetworkList.ps1
@@ -0,0 +1,28 @@
+function RemoveFrom-NetworkList
+{
+    Param(
+        [Parameter(Mandatory=$true)] [string] $NetworkListID,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)] [string] $Element,
+        [Parameter(Mandatory=$false)] [string] $EdgeRCFile = '~\.edgerc',
+        [Parameter(Mandatory=$false)] [string] $Section = 'default',
+        [Parameter(Mandatory=$false)] [string] $AccountSwitchKey
+    )
+
+    begin{}
+
+    process{
+        $Path = "/network-list/v2/network-lists/$NetworkListID/elements?element=$Element&accountSwitchKey=$AccountSwitchKey"
+
+        try {
+            $Result = Invoke-AkamaiRestMethod -Method DELETE -Path $Path -EdgeRCFile $EdgeRCFile -Section $Section
+            return $Result
+        }
+        catch {
+            throw $_.Exception
+        }
+    }
+
+    end{}
+    
+}
+
