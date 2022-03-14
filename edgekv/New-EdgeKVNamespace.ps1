6d91a5151589300efbc6ae13b0aa9f9a13d9e727
Stuart Macleod <heystuartmacleod@gmail.com>
2022-03-14 17:09:32 +0000

    Updating requirements

--- edgekv/New-EdgeKVNamespace.ps1
+++ edgekv/New-EdgeKVNamespace.ps1
@@ -2,16 +2,24 @@ function New-EdgeKVNamespace
 {
     Param(
         [Parameter(Mandatory=$true)]  [string] $Name,
+        [Parameter(Mandatory=$false)] [string] $RetentionInSeconds = 0,
+        [Parameter(Mandatory=$false)] [string] [ValidateSet('US','EU','JP')] $GeoLocation = 'US',
         [Parameter(Mandatory=$true)]  [string] [ValidateSet('STAGING','PRODUCTION')] $Network,
         [Parameter(Mandatory=$false)] [string] $EdgeRCFile = '~\.edgerc',
         [Parameter(Mandatory=$false)] [string] $Section = 'default',
         [Parameter(Mandatory=$false)] [string] $AccountSwitchKey
     )
 
+    if($Network -eq 'STAGING' -and $GeoLocation -ne 'US'){
+        throw 'Only valid GeoLocation for STAGING network is US currently'
+    }
+
     $Path = "/edgekv/v1/networks/$Network/namespaces?accountSwitchKey=$AccountSwitchKey"
 
     $BodyObj = @{
         name = $Name
+        geoLocation = $GeoLocation
+        retentionInSeconds = $RetentionInSeconds
     }
     $Body = $BodyObj | ConvertTo-Json
 
