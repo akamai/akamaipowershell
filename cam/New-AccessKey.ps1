8ef5723201fb863db26b5d1a3d02c9f8e37a5835
Stuart Macleod <heystuartmacleod@gmail.com>
2022-07-15 15:54:44 +0100

    Final endpoints

--- cam/New-AccessKey.ps1
+++ cam/New-AccessKey.ps1
@@ -1,20 +1,29 @@
 function New-AccessKey
 {
     Param(
-        [Parameter(Mandatory=$true)]  [string] $AccessKeyUID,
-        [Parameter(Mandatory=$true)]  [string] $Body,
+        [Parameter(Mandatory=$true,ParameterSetName='pipeline',ValueFromPipeline=$true)]  [string] $AccessKey,
+        [Parameter(Mandatory=$true,ParameterSetName='body')]  [string] $Body,
         [Parameter(Mandatory=$false)] [string] $EdgeRCFile = '~\.edgerc',
         [Parameter(Mandatory=$false)] [string] $Section = 'default',
         [Parameter(Mandatory=$false)] [string] $AccountSwitchKey
     )
 
-    $Path = "/cam/v1/access-keys/$AccessKeyUID`?accountSwitchKey=$AccountSwitchKey"
+    begin{}
 
-    try {
-        $Result = Invoke-AkamaiRestMethod -Method POST -Path $Path -Body $Body -EdgeRCFile $EdgeRCFile -Section $Section
-        return $Result
+    process{
+        $Path = "/cam/v1/access-keys?accountSwitchKey=$AccountSwitchKey"
+        if($AccessKey){
+            $Body = ConvertTo-Json $AccessKey -Depth 100
+        }
+
+        try {
+            $Result = Invoke-AkamaiRestMethod -Method POST -Path $Path -Body $Body -EdgeRCFile $EdgeRCFile -Section $Section
+            return $Result
+        }
+        catch {
+            throw $_.Exception
+        }
     }
-    catch {
-        throw $_.Exception
-    }
+
+    end{}
 }
