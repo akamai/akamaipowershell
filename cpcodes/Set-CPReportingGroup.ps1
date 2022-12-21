93e9c2f3a2963045b4ea5bffd3eec05aba3433a4
Stuart Macleod <heystuartmacleod@gmail.com>
2022-12-20 16:03:12 -0800

    Added testing, minor fixes

--- cpcodes/Set-CPReportingGroup.ps1
+++ cpcodes/Set-CPReportingGroup.ps1
@@ -2,21 +2,34 @@ function Set-CPReportingGroup
 {
     Param(
         [Parameter(Mandatory=$true)]  [string] $ReportingGroupID,
-        [Parameter(Mandatory=$true)]  [string] $Body,
+        [Parameter(Mandatory=$true,ParameterSetName='pipeline',ValueFromPipeline=$true)]  [object] $ReportingGroupObject,
+        [Parameter(Mandatory=$true,ParameterSetName='body')]  [string] $Body,
         [Parameter(Mandatory=$false)] [string] $EdgeRCFile = '~\.edgerc',
         [Parameter(Mandatory=$false)] [string] $Section = 'default',
         [Parameter(Mandatory=$false)] [string] $AccountSwitchKey
     )
-    
-    $Path = "/cprg/v1/reporting-groups/$ReportingGroupID`?accountSwitchKey=$AccountSwitchKey"
 
-    try {
-        $Result = Invoke-AkamaiRestMethod -Method PUT -Path $Path -Body $Body -EdgeRCFile $EdgeRCFile -Section $Section
-        return $Result
+    begin{}
+
+    process{
+        if($ReportingGroupObject){
+            $Body = ConvertTo-Json -Depth 10 $ReportingGroupObject
+        }
+
+        $Path = "/cprg/v1/reporting-groups/$ReportingGroupID`?accountSwitchKey=$AccountSwitchKey"
+
+        try {
+            $Result = Invoke-AkamaiRestMethod -Method PUT -Path $Path -Body $Body -EdgeRCFile $EdgeRCFile -Section $Section
+            return $Result
+        }
+        catch {
+            throw $_
+        }
     }
-    catch {
-        throw $_
-    }
+
+    end{}
+    
+    
 }
 
 # SIG # Begin signature block
