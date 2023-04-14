c2f8a8a3879f66d570218b199aed72402f5b955d
Benjamin Brouard <bbrouard@akamai.com>
2023-04-14 12:08:54 +0200

    Fix path and required parameters

--- appsec/List-AppSecSelectableHostnames.ps1
+++ appsec/List-AppSecSelectableHostnames.ps1
@@ -1,33 +1,18 @@
 function List-AppSecSelectableHostnames
 {
     Param(
-        [Parameter(ParameterSetName="name", Mandatory=$true)]  [string] $ConfigName,
-        [Parameter(ParameterSetName="id", Mandatory=$true)]    [string] $ConfigID,
-        [Parameter(Mandatory=$true)]  [string] $VersionNumber,
+        [Parameter(Mandatory=$true)]  [string] $GroupId,
+        [Parameter(Mandatory=$true)]  [string] $ContractId,
         [Parameter(Mandatory=$false)] [string] $EdgeRCFile,
         [Parameter(Mandatory=$false)] [string] $Section,
         [Parameter(Mandatory=$false)] [string] $AccountSwitchKey
     )
 
-    if($ConfigName){
-        $Config = List-AppSecConfigurations -EdgeRCFile $EdgeRCFile -Section $Section -AccountSwitchKey $AccountSwitchKey | where {$_.name -eq $ConfigName}
-        if($Config){
-            $ConfigID = $Config.id
-        }
-        else{
-            throw("Security config '$ConfigName' not found")
-        }
-    }
-
-    if($VersionNumber.ToLower() -eq 'latest'){
-        $VersionNumber = (List-AppSecConfigurationVersions -ConfigID $ConfigID -PageSize 1 -EdgeRCFile $EdgeRCFile -Section $Section -AccountSwitchKey $AccountSwitchKey).version
-    }
-
-    $Path = "/appsec/v1/configs/$ConfigID/versions/$VersionNumber/selectable-hostnames"
+    $Path = "/appsec/v1/contracts/$ContractId/groups/$GroupId/selectable-hostnames"
 
     try {
         $Result = Invoke-AkamaiRestMethod -Method GET -Path $Path -EdgeRCFile $EdgeRCFile -Section $Section -AccountSwitchKey $AccountSwitchKey
-        return $Result.selectedSet
+        return $Result.availableSet
     }
     catch {
         throw $_ 
